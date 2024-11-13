f7421bea4de2cf9503471528f0e188d916e8fcdc
Olaf Cichosz <01171645@pw.edu.pl>
2024-11-13 22:24:06 +0100

    zadanie

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -2,8 +2,29 @@
 
 #include "Stocznia.hpp"
 
+// Funkcja transportujFlota
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0) {
+        return 0; // Jeœli iloœæ towaru wynosi 0, zwracamy 0 bez tworzenia statków
+    }
+
+    Stocznia     stocznia;               // Fabryka statków
+    unsigned int przetransportowane = 0; // Licznik przetransportowanego towaru
+    unsigned int liczbaZaglowcow    = 0; // Licznik ¿aglowców
+
+    while (przetransportowane < towar) {
+        Statek*      statek = stocznia();            // Tworzymy losowy statek
+        unsigned int ilosc  = statek->transportuj(); // Transportujemy towar przy u¿yciu statku
+        przetransportowane += ilosc; // Dodajemy iloœæ przetransportowanego towaru do sumy
+
+        // Sprawdzamy, czy stworzony statek jest ¿aglowcem
+        if (dynamic_cast< Zaglowiec* >(statek)) {
+            liczbaZaglowcow++; // Je¿eli to ¿aglowiec, zwiêkszamy licznik ¿aglowców
+        }
+
+        delete statek; // Usuwamy dynamicznie utworzony statek
+    }
+
+    return liczbaZaglowcow; // Zwracamy liczbê ¿aglowców
 }
